892cc3576905627a870fc53ea3b04fa7a737f14f
aditya <adityasarda08.com>
2026-05-10 18:37:03 +0530

    Updated Api Url commit

--- frontend/src/components/LeadForm.jsx
+++ frontend/src/components/LeadForm.jsx
@@ -23,7 +23,7 @@ const LeadForm = ({ fetchLeads }) => {
     }
 
     try {
-      await fetch('http://localhost:5000/api/leads', {
+      await fetch('https://lrm-xm86.onrender.com/api/leads', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData)
